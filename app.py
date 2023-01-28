7ef6d18b28167de49281be550b16af7a28c0d595
Víctor Andrés Rojas <victandres98@gmail.com>
2023-01-28 16:01:58 -0500

    Creating User and Post Models

--- app.py
+++ app.py
@@ -7,5 +7,57 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='profile.jpg')
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+posts = [
+    {
+        'autor': 'Jhon Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2023'
+    },
+    {
+        'autor': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2023'
+    },
+    {
+        'autor': 'Victor Doe',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'April 22, 2023'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
