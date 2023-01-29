7b9b42653600c6dedb566442b7769a5dad5651fa
Víctor Andrés Rojas <victandres98@gmail.com>
2023-01-29 12:57:22 -0500

    Signup and Singin forms - Using wtforms to create forms with flask - Adding Validators to form inputs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -57,6 +58,27 @@ posts = [
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@localhost.com' and form.password.data == 'Password-123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
